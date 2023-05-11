c051a3751c029cc0b2ba10f7f2e57bccaee3b2e6
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-05-11 23:43:25 +0530

    Header Fix

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,8 +4,15 @@ import "./Header.css";
 function Header(props) {
   return (
     <div className=" Header-div ">
-      <i className="bx bx-menu"></i>
-      <i className="bx bxs-user-circle"></i>
+      <div className="Header-div__top">
+        <i className="bx bxs-bank"></i>
+        <i className="bx bx-stats"></i>
+        <i className="bx bxs-category"></i>
+      </div>
+      <div className="Header-div__bottom">
+        <i className="bx bxs-user-circle"></i>
+        <i className="bx bxs-log-out"></i>
+      </div>
     </div>
   );
 }
