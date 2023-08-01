1587ac8fb219c9140584d5062c8dc130c2654b25
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-08-01 13:16:11 +0530

    B: encrypting the password

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,9 +1,15 @@
 const User = require("../models/user")
+const bcrypt = require("bcrypt")
+const saltRound = 10
 
 exports.create = async (req, res) => {
     try {
         const { email, password } = req.body
-        const dbRes = await User.create({ email, password })
+
+        const hash = await bcrypt.hash(password, saltRound)
+
+        const dbRes = await User.create({ email, password: hash })
+
         res.send({ error: false, body: dbRes.dataValues })
     } catch (error) {
         console.log(error);
@@ -16,16 +22,19 @@ exports.create = async (req, res) => {
 exports.get = async (req, res) => {
     try {
         const { email, password } = req.body
-        const dbRes = await User.findOne({
-            where: {
-                email: email, password: password
-            }
-        })
+        const dbRes = await User.findOne({ where: { email: email } })
 
         if (dbRes === null) {
             res.send({ error: "User Not Found", body: null })
             return;
         }
+
+        const comparesionRes = await bcrypt.compare(password, dbRes.password)
+        if (!comparesionRes) {
+            res.send({ error: "Password Not Matched", body: null })
+            return;
+        }
+
 
         res.send({ error: false, body: dbRes.dataValues })
     } catch (error) {
